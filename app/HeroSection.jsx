a6339960a2ff5a57aafa77b921918978e56d46c4
Fabien <chevallereafab@gmail.com>
2026-02-05 17:29:20 +0100

    Test Performances Without Loader

--- app/HeroSection.jsx
+++ app/HeroSection.jsx
@@ -7,8 +7,7 @@ import Hero from './hero'
 
 export default function HeroSection() {
   return (
-    <Loader>
+
       <Hero />
-    </Loader>
   )
 }
--- app/Sections.jsx
+++ app/Sections.jsx
@@ -3,13 +3,13 @@
 
 import dynamic from 'next/dynamic'
 import {useRef} from 'react'
-const AboutUs = dynamic(() => import('./about_us_section'))
+const AboutUs = dynamic(() => import('./about_us_section'),{srr:false})
 const Projects = dynamic(() => import('./projects'))
 const Team = dynamic(() => import('./team_section'),{ssr:false})
 const Contact = dynamic(() => import('./contact_section'))
 const Interaction = dynamic(() => import('./interaction'),{srr:false})
 const Pricing = dynamic(() => import('./pricing'))
-const Footer = dynamic(() => import('./footer_section'))
+const Footer = dynamic(() => import('./footer_section'),{srr:false})
 
 export default function Sections() {
   const contactRef = useRef(null)
